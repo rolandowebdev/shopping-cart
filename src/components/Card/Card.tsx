8cf7ad9389ad15fee16ea9ca08e1fa0161f6f596
RolandoPranataAR <rolandopranata@gmail.com>
2023-03-17 21:17:22 +0700

    feat: create button increase and decrease quantity

--- src/components/Card/Card.tsx
+++ src/components/Card/Card.tsx
@@ -8,7 +8,9 @@ import {
   Image,
   Stack,
   Text,
+  Flex,
 } from '@chakra-ui/react'
+import { AddIcon, MinusIcon } from '@chakra-ui/icons'
 import { formatCurrency } from '@/utilities/formatCurrency'
 
 type CardProps = {
@@ -19,32 +21,49 @@ type CardProps = {
   imgUrl: string
 }
 
-export const Card = ({ id, name, description, price, imgUrl }: CardProps) => (
-  <CardChakra bg="blackAlpha.600" borderRadius="lg" overflow="hidden">
-    <CardBody>
-      <Image
-        src={imgUrl}
-        h="220px"
-        w="full"
-        objectFit="cover"
-        alt="Green double couch with wooden legs"
-      />
-      <Stack mt="6" spacing="3" px="2">
-        <Heading as="h2" fontSize="2xl" fontWeight="bold">
-          {name}
-        </Heading>
-        <Text>{id}</Text>
-        <Text>{description}</Text>
-        <Text color="slateblue" fontSize="2xl">
-          {formatCurrency(price)}
-        </Text>
-      </Stack>
-    </CardBody>
-    <Divider />
-    <CardFooter px="2" py="4">
-      <Button py={2} px={4} bg="slateblue" w="full" rounded="lg">
-        Add to cart
-      </Button>
-    </CardFooter>
-  </CardChakra>
-)
+export const Card = ({ id, name, description, price, imgUrl }: CardProps) => {
+  const quantity = 1
+  return (
+    <CardChakra bg="blackAlpha.600" borderRadius="lg" overflow="hidden">
+      <CardBody>
+        <Image
+          src={imgUrl}
+          h="220px"
+          w="full"
+          objectFit="cover"
+          alt="Green double couch with wooden legs"
+        />
+        <Stack mt="6" spacing="3" px="2">
+          <Heading as="h2" fontSize="2xl" fontWeight="bold">
+            {name}
+          </Heading>
+          <Text>{id}</Text>
+          <Text>{description}</Text>
+          <Text color="slateblue" fontSize="2xl">
+            {formatCurrency(price)}
+          </Text>
+        </Stack>
+      </CardBody>
+      <Divider />
+      <CardFooter px="2" py="4">
+        {quantity < 1 ? (
+          <Button py={2} px={4} bg="slateblue" w="full" rounded="lg">
+            Add to cart
+          </Button>
+        ) : (
+          <Flex alignItems="center" gap={4} mx="auto">
+            <Button bg="slateblue" p={2}>
+              <AddIcon />
+            </Button>
+            <Text as="span" fontSize="xl" fontWeight="bold">
+              {quantity}
+            </Text>
+            <Button bg="slateblue" p={2}>
+              <MinusIcon />
+            </Button>
+          </Flex>
+        )}
+      </CardFooter>
+    </CardChakra>
+  )
+}
