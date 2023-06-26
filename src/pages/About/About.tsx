4ce73d8a82fb1f04e6ebb120c7bb242de17360d3
RolandoPranataAR <rolandopranata@gmail.com>
2023-06-26 13:45:14 +0700

    chore: styling title and link

--- src/pages/About/About.tsx
+++ src/pages/About/About.tsx
@@ -1,3 +1,7 @@
 import { Text } from '@chakra-ui/react'
 
-export const About = () => <Text as="h1">About</Text>
+export const About = () => (
+  <Text as="h1" fontSize="3xl" fontWeight="semibold">
+    About
+  </Text>
+)
--- src/pages/Home/Home.tsx
+++ src/pages/Home/Home.tsx
@@ -3,7 +3,14 @@ import { Text } from '@chakra-ui/react'
 
 export const Home = () => (
   <>
-    <Text as="h1">Home</Text>
-    <Link to="/store">Go to store page</Link>
+    <Text as="h1" fontSize="3xl" fontWeight="semibold">
+      Home
+    </Text>
+    <Text
+      as={Link}
+      to="/store"
+      _hover={{ textDecoration: 'underline', color: 'blue.300' }}>
+      Go to store page
+    </Text>
   </>
 )
